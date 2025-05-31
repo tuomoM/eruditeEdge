7dee582e253adcc2b1df19b8fe359542b590b315
tuomo <tuomo.mehtala@gmail.com>
2025-05-31 21:20:17 +0300

    Flashcards work, but moving the result to flashcards does not.

--- views/train_fl.py
+++ views/train_fl.py
@@ -9,5 +9,6 @@ def train_fl(vocabs):
     print(len(vocabs))
     if not session["user_id"]:
         return redirect("/")
-  
+    user_id = session["user_id"]
+    vocabs = vocab_service.get_vocabs(user_id)
     return render_template("practiceFlash.html", vocabs = vocabs)
